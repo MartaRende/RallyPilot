54b2fd3ebf69e108049fb7078eff55751ee34c4b
marttave <martin.tavernier256@gmail.com>
2024-10-25 08:22:52 +0000

    wip

--- federated_training.py
+++ federated_training.py
@@ -17,12 +17,12 @@ print("Devide for torch is : " + str(device))
 print(f"Initialized {len(clients)} clients")
 
 
-CURR_MODEL_NAME = "5_forward_24_back"
+CURR_MODEL_NAME = "6_forward_27_back"
 
-LOSS_FN_WEIGHT = torch.tensor([0.5, 2.4, 1, 1])
+LOSS_FN_WEIGHT = torch.tensor([0.6, 2.7, 1, 1])
 LOSS_FN_WEIGHT = LOSS_FN_WEIGHT.to(device)
 LEARNING_RATE = 0.001
-N_EPOCH = 30
+N_EPOCH = 40
 
 
 def printEpochAcc(epochAcc: list[dict[int, float]]):
