54a40443c7e45696ee705bfe6a77d7969bff1e72
marttave <martin.tavernier256@gmail.com>
2024-11-02 10:12:35 +0000

    Added too close for confort

--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import pickle
 import lzma
 import matplotlib.pyplot as plt
@@ -30,18 +30,18 @@ def getSpeedStats(fd, lcl):
     plt.savefig(f"{STATS_FOLDER}speed.png")
 
 
-def getTotalDistances(fd, lcl):
+def getMeanDistances(fd, lcl):
     totalDistances = {"fd": [], "lcl": []}
     for i in range(len(fd)):
-        totalDistances["fd"].append(sum(fd[i].raycast_distances))
-        totalDistances["lcl"].append(sum(lcl[i].raycast_distances))
+        totalDistances["fd"].append(mean(fd[i].raycast_distances))
+        totalDistances["lcl"].append(mean(lcl[i].raycast_distances))
     return totalDistances
 
 
 def getSomeDistances(fd, lcl):
 
     def getRays(arr):
-        return [arr[0], arr[6], arr[14]]
+        return [arr[6]]
 
     totalDistances = {"fd": [], "lcl": []}
     for i in range(len(fd)):
@@ -55,9 +55,9 @@ def mean(arr):
 
 
 def getRayGraphs():
-    res = getTotalDistances(fd_data, lcl_data)
+    res = getMeanDistances(fd_data, lcl_data)
 
-    print(f"Mean fd : {mean(res['fd'])} Mean lcl: {mean(res['lcl'])}")
+    print(f"Mean distance fd : {mean(res['fd'])} Mean distance lcl: {mean(res['lcl'])}")
 
     plt.figure()
     plt.plot(range(len(res["fd"])), res["fd"])
@@ -66,5 +66,44 @@ def getRayGraphs():
     plt.savefig(f"{STATS_FOLDER}distances.png")
 
 
+def countTooCloseForConfort(fd, lcl):
+    TRESH = 5
+    count = {"fd": 0, "lcl": 0}
+    counts = {"fd": [0], "lcl": [0]}
+    for i in range(len(fd)):
+        addFd = False
+        addLcl = False
+        for j in range(len(fd[i].raycast_distances)):
+            if fd[i].raycast_distances[j] <= TRESH:
+                addFd = True
+            if lcl[i].raycast_distances[j] <= TRESH:
+                addLcl = True
+        currLcl = counts["lcl"][-1]
+        currFd = counts["fd"][-1]
+        if addFd:
+            counts["fd"].append(currFd + 1)
+        else:
+            counts["fd"].append(currFd)
+        if addLcl:
+            counts["lcl"].append(currLcl + 1)
+        else:
+            counts["lcl"].append(currLcl)
+    return counts
+
+
+def graphsTooClose():
+    res = countTooCloseForConfort(fd_data, lcl_data)
+    plt.figure()
+    plt.plot(range(len(res["fd"])), res["fd"])
+    plt.plot(range(len(res["lcl"])), res["lcl"])
+    plt.legend(["Federated", "Basic"])
+    plt.title("Too close for confort counts")
+    plt.xlabel("Frames")
+    plt.ylabel("Counts")
+    plt.savefig(f"{STATS_FOLDER}tooClose.png")
+    pass
+
+
 getSpeedStats(fd_data, lcl_data)
 getRayGraphs()
+graphsTooClose()
